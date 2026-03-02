ae2cc4dbad1bcff203400fec41d2d303b3938356
Xavi-brom <212230@student.aventus.nl>
2026-03-02 11:35:06 +0100

    beter nu werk je

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+	preview: {
+		allowedHosts: ['svelte-project-qgfv.onrender.com'],
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
